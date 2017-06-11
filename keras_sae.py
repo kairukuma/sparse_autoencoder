15e1c05c34ce87ccc2a0b0ed82fc4e74574e3cf2
Kyle Xiong <kylexiong1993@gmail.com>
2017-06-11 17:20:43 -0400

    Added SAE script from JEFFERYF BOBW with modifications for TF 1.1.0 on Windows 10 64-bit.

--- keras_sae.py
+++ keras_sae.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,6 +8,8 @@ from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras import backend as K
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 (x_train, _), (x_test, _) = mnist.load_data()
 
 encode_dim = 32
@@ -14,7 +17,7 @@ encode_dim = 32
 input_img = Input(shape=(784,))  # adapt this if using `channels_first` image data format
 
 encoded = Dense(encode_dim, activation='relu',
-	activity_regularizer=regularizers.l1(1e-4))(input_img)
+	activity_regularizer=regularizers.l1(1e-5))(input_img)
 decoded = Dense(28**2, activation = 'sigmoid')(encoded)
 
 autoencoder = Model(input_img, decoded)
@@ -48,13 +51,17 @@ decoded_imgs = decoder.predict(encoded_imgs)
 print(encoded_imgs.mean())
 
 enc_W = encoder.get_weights()[0]
+feature_images = np.zeros(enc_W.shape)
+
+for i in range(feature_images.shape[1]):
+	feature_images[:,i] = enc_W[:,i] / np.linalg.norm(enc_W)
 
 n = 32
 plt.figure()
 
 for i in range(n):
 	ax = plt.subplot(4, 8, i + 1)
-	plt.imshow(enc_W[:,i].reshape(28, 28))
+	plt.imshow(feature_images[:,i].reshape(28, 28))
 	plt.gray()
 	ax.get_xaxis().set_visible(False)
 	ax.get_yaxis().set_visible(False)
